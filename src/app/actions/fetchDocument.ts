b8b3e7722ae13402b942ff28d0200c87044c594f
Kevin Foley <kevin.james.foley@gmail.com>
2023-10-11 15:57:31 +0100

    run prettier

--- src/app/actions/fetchDocument.ts
+++ src/app/actions/fetchDocument.ts
@@ -1,8 +1,8 @@
-import { generateDocumentID } from '@/app/actions/generateDocumentID';
-import { kv } from '@vercel/kv'
+import { generateDocumentID } from "@/app/actions/generateDocumentID";
+import { kv } from "@vercel/kv";
 
 export async function fetchDocument(userId: string, portalName: string) {
-    const id = await generateDocumentID(userId, portalName);
-    const document = await kv.get<string>(id);
-    return document ?? 'Hello, World!';
+  const id = await generateDocumentID(userId, portalName);
+  const document = await kv.get<string>(id);
+  return document ?? "Hello, World!";
 }
--- src/app/edit/page.tsx
+++ src/app/edit/page.tsx
@@ -4,7 +4,11 @@ import { getContent } from '@/app/actions/fetchCopilotData';
 import { API_KEYS } from '@/app/actions/getApiKey';
 import { fetchDocument } from '@/app/actions/fetchDocument';
 
-export default async function Edit({ searchParams }:  { searchParams: SearchParams }) {
+export default async function Edit({
+  searchParams,
+}: {
+  searchParams: SearchParams;
+}) {
   const apiKey = API_KEYS[searchParams.name as PageName];
   if (!apiKey) return <ErrorComponent msg="You do not have the correct API credentials to run this custom app"/>;
   const data = await getContent(searchParams, apiKey)
@@ -12,5 +16,5 @@ export default async function Edit({ searchParams }:  { searchParams: SearchPara
   const document = await fetchDocument(data.me.id, data.me.portalName) ?? 'Hello, World!';
   return (
     <EditClient document={document} name={searchParams.name as PageName} />
-  )
+  );
 }
