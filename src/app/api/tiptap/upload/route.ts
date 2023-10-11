c308ff7e7581a38570b2925dff5681486af32f7a
Aymen Hamzaoui <elhamzaouiaymen@gmail.com>
2023-10-11 16:44:46 +0100

    fix: typescript errors

--- src/app/api/tiptap/upload/route.ts
+++ src/app/api/tiptap/upload/route.ts
@@ -1,13 +1,24 @@
-import { put } from "@vercel/blob";
-import { NextResponse } from "next/server";
-
+import { put } from '@vercel/blob';
+import { NextResponse } from 'next/server';
+ 
 export async function POST(request: Request): Promise<NextResponse> {
   const { searchParams } = new URL(request.url);
-  const filename = searchParams.get("filename");
+  const filename = searchParams.get('filename');
+  if(!filename) {
+    return NextResponse.json({
+      error: 'filename is required',
+    });
+  }
 
-  const blob = await put(filename, request.body, {
-    access: "public",
+  if(!request?.body) {
+    return NextResponse.json({
+      error: 'body is required',
+    });
+  }
+ 
+  const blob = await put(filename, request?.body, {
+    access: 'public',
   });
-
+ 
   return NextResponse.json(blob);
 }
