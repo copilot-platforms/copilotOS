4337771f0867b87157e15bfaab38ae137b9b359d
Kevin Foley <kevin.james.foley@gmail.com>
2023-10-11 12:09:37 +0100

    cleanup

--- src/app/view/page.tsx
+++ src/app/view/page.tsx
@@ -1,7 +1,5 @@
-import { fetchDocument } from '@/app/actions/fetchDocument'
 import { View as ViewComponent } from '@/components/View'
-import { putDocument } from '../actions/putDocument';
-import { fetchDocumentById } from '../actions/fetchDocumentById';
+import { fetchDocumentById } from '@/app/actions/fetchDocumentById';
 
 export default async function View() {
     const document = await fetchDocumentById('testing123');
