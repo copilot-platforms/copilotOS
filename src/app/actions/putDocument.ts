6f4dcb2db991dab508e094a3898568f75d141174
Kevin Foley <kevin.james.foley@gmail.com>
2023-10-11 15:40:28 +0100

    fix the put document endpoint

--- src/app/actions/putDocument.ts
+++ src/app/actions/putDocument.ts
@@ -2,8 +2,13 @@
 
 import { generateDocumentID } from '@/app/actions/generateDocumentID';
 import { kv } from '@vercel/kv'
+import { getContent } from '@/app/actions/fetchCopilotData';
+import { API_KEYS } from './getApiKey';
 
-export async function putDocument(userId: string, portalName: string) {
-    const id = await generateDocumentID(userId, portalName);
-    await kv.set(id, document);
+export async function putDocument(name: string, document: string) {
+  const apiKey = API_KEYS[name as PageName];
+  const data = await getContent({}, apiKey);
+  if (!data.me) throw new Error('could not fetch data');
+  const id = await generateDocumentID(data.me.id, data.me.portalName);
+  await kv.set(id, document);
 }
